f68c802c2bf052e4c40130f83cb6a2d05807713a
Tafita Mathieu <199898191+Mathieu-bot@users.noreply.github.com>
2026-09-09 05:54:44 +0300

    chore:  HuoCodeApplication -> PojaApplication

--- src/main/java/io/huocode/api/PojaApplication.java
+++ src/main/java/io/huocode/api/PojaApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @PojaGenerated
-public class HuoCodeApplication {
+public class PojaApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(HuoCodeApplication.class, args);
+    SpringApplication.run(PojaApplication.class, args);
   }
 }
